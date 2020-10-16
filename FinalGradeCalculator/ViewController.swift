0e2ebd3003abebda2d69fc50aec2f40387918013
aleneweaver11 <aleneweaver11@gmail.com>
2020-10-16 10:03:06 -0400

    Initial Commit

---
+++ FinalGradeCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalGradeCalculator
+//
+//  Created by  on 10/16/20.
+//  Copyright © 2020 AubreyApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
